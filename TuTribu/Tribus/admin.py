bc1fd75173fdfcb0d59e0f1bc809b699155f4c32
Sonia <samoreno825@hotmail.com>
2021-10-05 15:13:14 -0500

    Comentario

--- TuTribu/Tribus/admin.py
+++ TuTribu/Tribus/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Tribus.models import *
+
 
 # Register your models here.
+admin.site.register(Tribu)
+admin.site.register(Post)
+admin.site.register(Reaccion)
+admin.site.register(ReaccionPost)
--- TuTribu/Tribus/views.py
+++ TuTribu/Tribus/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from Tribus.serializers import *
 
 # Create your views here.
+
+class TribuAPI(viewsets.ModelViewSet):
+    serializer_class = TribuSerial
+    queryset = Tribu.objects.all() #Contiene los objetos que se desean enviar a través del API
+
+class PostAPI(viewsets.ModelViewSet):
+    serializer_class = PostSerial
+    queryset = Post.objects.all()
+
+class ReaccionAPI(viewsets.ModelViewSet):
+    serializer_class = ReaccionSerial
+    queryset = Reaccion.objects.all()
+
+class ReaccionPostAPI(viewsets.ModelViewSet):
+    serializer_class = ReaccionPostSerial
+    queryset = ReaccionPost.objects.all()
+    
+
+
+
+
+
+
+    
